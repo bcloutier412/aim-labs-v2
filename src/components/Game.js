b6b8807b3fc4ebd21157c184fd9e647078342051
Dino <brandoncloutier412@gmail.com>
2022-08-02 00:22:13 -0700

    implemented the target onClick funtionality to rerender the targets x and y

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,46 +1,75 @@
 import React from "react";
 import "./Game.css";
 
+/*
+  Target Component
+  @Desc: this component renders a div with the class of .target and onclick will;
+  change its state to generate a new location on the screen, execute functions to increase score, 
+  change accuracy, increase total shots.
+*/
 class Target extends React.Component {
   getRandomNum(num, targetDiameter) {
-    return (Math.floor(Math.random() * (num - 0)) + 0) * targetDiameter
+    return (Math.floor(Math.random() * (num - 0)) + 0) * targetDiameter;
   }
   constructor(props) {
     super(props);
-    const numOfCols = Math.floor((window.innerWidth * 0.8) / this.props.targetDiameter);
-    const numOfRows = Math.floor((window.innerHeight * 0.9) / this.props.targetDiameter);
+    this.numOfCols = Math.floor(
+      (window.innerWidth * 0.8) / this.props.targetDiameter
+    );
+    this.numOfRows = Math.floor(
+      (window.innerHeight * 0.9) / this.props.targetDiameter
+    );
     this.state = {
-      top: this.getRandomNum(numOfRows, this.props.targetDiameter),
-      left: this.getRandomNum(numOfCols, this.props.targetDiameter)
+      top: this.getRandomNum(this.numOfRows, this.props.targetDiameter),
+      left: this.getRandomNum(this.numOfCols, this.props.targetDiameter),
     };
   }
   render() {
-    const targetDiameter = this.props.targetDiameter;
     return (
       <div
         style={{
-          height: `${targetDiameter - 4}px`,
-          width: `${targetDiameter - 4}px`,
+          height: `${this.props.targetDiameter - 4}px`,
+          width: `${this.props.targetDiameter - 4}px`,
           backgroundColor: `${this.props.targetColor}`,
           top: `${this.state.top}px`,
-          left: `${this.state.left}px`
+          left: `${this.state.left}px`,
         }}
         className="target"
+        onClick={(e) => {
+          const newTop = this.getRandomNum(
+            this.numOfRows,
+            this.props.targetDiameter
+          );
+          const newLeft = this.getRandomNum(
+            this.numOfCols,
+            this.props.targetDiameter
+          );
+          this.setState({
+            top: newTop,
+            left: newLeft,
+          });
+          e.stopPropagation();
+        }}
       ></div>
     );
   }
 }
 
+/*
+  Game Component
+  @Desc: This component
+*/
 class Game extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       inPlay: false,
+      totalShots: 0,
+      targetsHit: 0,
+      accuracy: 0,
     };
   }
-  startPlay() {
-    
-  }
+  startPlay() {}
   render() {
     const array = [];
     for (let i = 0; i < 7; i++) {
@@ -53,7 +82,11 @@ class Game extends React.Component {
         />
       );
     }
-    return <div className="game-board">{array}</div>;
+    return (
+      <div className="game-board" onClick={() => {}}>
+        {array}
+      </div>
+    );
   }
 }
 export default Game;
