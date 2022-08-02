6d7efca3327e46a15b9a1a8fd5ad0cfaf8194c1f
Dino <brandoncloutier412@gmail.com>
2022-08-02 10:01:14 -0700

    restyled the header.css, implemented the html/css and react structure for the in-game stats

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "./Game.css";
 
-
 //HELPER FUNCTIONS
 /*
   @Desc: returns a random number based on the number of cols or rows and the target diameter
@@ -41,6 +40,9 @@ class Target extends React.Component {
         }}
         className="target"
         onClick={(e) => {
+          this.props.increaseTotalShots();
+          this.props.increaseTargetsHit();
+          this.props.increaseScore();
           const newTop = getRandomNum(
             this.numOfRows,
             this.props.targetDiameter
@@ -53,8 +55,6 @@ class Target extends React.Component {
             top: newTop,
             left: newLeft,
           });
-          this.props.increaseTotalShots()
-          this.props.increaseTargetsHit()
           e.stopPropagation();
         }}
       ></div>
@@ -63,6 +63,34 @@ class Target extends React.Component {
 }
 
 /*
+  In Game Stats Component
+  @Desc: This component will render the time, score, and accuracy variables. It also houses the funtionality of the timer
+
+*/
+class InGameStats extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      minute: this.props.minute,
+      second: 0,
+    };
+  }
+  formatSeconds(second) {
+    if (second < 10) {
+      return '0' + second
+    } 
+    return second
+  }
+  render() {
+    return (
+      <div className="in-game-stats">
+        {this.state.minute}:{this.formatSeconds(this.state.second)} | {this.props.score} |{" "}
+        {this.props.accuracy}%
+      </div>
+    );
+  }
+}
+/*
   Game Component
   @Desc: This component holds all the game data and handles the renders for said data to the dashbaord. 
   After the game is over this component will also instigate the end game statistics to populate
@@ -70,8 +98,8 @@ class Target extends React.Component {
 class Game extends React.Component {
   constructor(props) {
     super(props);
-    this.totalShots = 0
-    this.targetsHit = 0
+    this.totalShots = 0;
+    this.targetsHit = 0;
     this.state = {
       inPlay: false,
       accuracy: 0,
@@ -79,10 +107,13 @@ class Game extends React.Component {
     };
   }
   increaseTotalShots() {
-    this.totalShots += 1
+    this.totalShots += 1;
   }
   increaseTargetsHit() {
-    this.targetsHit += 1
+    this.targetsHit += 1;
+  }
+  increaseScore() {
+    this.setState({ score: this.state.score + 1000 });
   }
   startPlay() {}
   render() {
@@ -96,13 +127,23 @@ class Game extends React.Component {
           key={i}
           increaseTotalShots={this.increaseTotalShots.bind(this)}
           increaseTargetsHit={this.increaseTargetsHit.bind(this)}
-
+          increaseScore={this.increaseScore.bind(this)}
         />
       );
     }
     return (
-      <div className="game-board" onClick={this.increaseTotalShots.bind(this)}>
-        {array}
+      <div>
+        <InGameStats
+          score={this.state.score}
+          accuracy={this.state.accuracy}
+          minute={this.props.minute}
+        />
+        <div
+          className="game-board"
+          onClick={this.increaseTotalShots.bind(this)}
+        >
+          {array}
+        </div>
       </div>
     );
   }
